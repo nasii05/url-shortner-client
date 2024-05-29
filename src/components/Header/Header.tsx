2e649bed887eea3ea22e9a88c08ef668620cf4ca
nasii05 <iamnasih@gmail.com>
2024-05-29 19:52:04 +0530

    header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface IHeaderProps{
+
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = () => {
+  return (
+    <div className='bg-slate-900'>
+        <div className='container p-2'>
+
+        </div>
+    </div>
+  )
+};
+
+export default Header;
