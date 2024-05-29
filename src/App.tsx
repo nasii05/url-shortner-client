440715df755ae75d2641fe4e7f754cfd32305db1
nasii05 <iamnasih@gmail.com>
2024-05-29 19:55:35 +0530

    header nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import * as React from "react";
+import Header from "./components/Header/Header";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+interface IAppProps {}
+
+const App: React.FunctionComponent<IAppProps> = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;
