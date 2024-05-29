f7ad6b705f82f6625692cff30b1f61089a2a99fc
nasii05 <iamnasih@gmail.com>
2024-05-29 19:59:11 +0530

    footer initialized

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IFooterProps {
+}
+
+const Footer: React.FunctionComponent<IFooterProps> = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+};
+
+export default Footer;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FunctionComponent<IHeaderProps> = () => {
     <div className="bg-slate-900">
       <div className="container p-2 mx-auto">
         <nav className="py-5">
-          <div className="text-base text-white">URLShortner</div>
+          <div className="text-base text-white ml-8">URLShortner</div>
         </nav>
       </div>
     </div>
